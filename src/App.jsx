5ce1ced7ddfca782fdece1709e09e627b7109d1e
Kelvin <knjuiri@gmail.com>
2025-05-07 16:26:03 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "../src/components/Button";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.2rem] overflow-hidden">
+        <Header />
+        <Hero />
+      </div>
+      <ButtonGradient />
+    </>
+  );
+};
+export default App;
